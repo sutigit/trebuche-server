57ce17a3dbe6428298234dede98c3ac8c1f1cad7
sutigit <panyanilben@gmail.com>
2025-07-21 14:55:26 +0300

    add pg

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
