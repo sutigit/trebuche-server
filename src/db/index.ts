eaa46189fa4e7305eaa603281fb20c04f0bc17d0
sutigit <panyanilben@gmail.com>
2025-07-26 23:03:45 +0300

    add types

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
+import { Database } from "./types";
 
 dotenv.config();
 
 const supabaseUrl = "https://vutuqhljydsjjynubjla.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY || "";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
