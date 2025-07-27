b4c3155b4f0f96fb60a4fbe7b440b1bfd9b4afcd
sutigit <panyanilben@gmail.com>
2025-07-27 10:31:46 +0300

    auth

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
+import auth from "./routes/auth";
 import users from "./routes/users";
 import bots from "./routes/bots";
 import conversations from "./routes/conversations";
@@ -14,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/auth", auth);
 app.use("/users", users);
 app.use("/bots", bots);
 app.use("/conversations", conversations);
---
+++ src/routes/auth.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import db from "../db/index";
+
+const router = express.Router();
+
+router.post("/login", async (req: Request, res: Response) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  let { data, error } = await db.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+  return res.status(200).json({ data });
+});
+
+router.post("/logout", async (req: Request, res: Response) => {
+  let { error } = await db.auth.signOut();
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+  return res.status(200).send();
+});
+
+export default router;
