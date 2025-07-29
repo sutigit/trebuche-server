1c29fdaeb3c26189fcc98e79d161db1e003ebe51
sutigit <panyanilben@gmail.com>
2025-07-29 21:51:43 +0300

    bot response endpoint

--- src/routes/openai.ts
+++ src/routes/openai.ts
@@ -6,7 +6,7 @@ import openai from "../lib/openai";
 dotenv.config();
 const router = express.Router();
 
-router.post("/", async (req: Request, res: Response) => {
+router.post("/basic", async (req: Request, res: Response) => {
   const content = req.body.content;
   const response = await openai.responses.create({
     model: process.env.OPENAI_MODEL,
@@ -15,7 +15,32 @@ router.post("/", async (req: Request, res: Response) => {
 
   const output = response.output_text;
 
-  return res.status(200).json({ message: output });
+  return res.status(200).json({ response: output });
+});
+
+router.post("/bot/:id", async (req: Request, res: Response) => {
+  const bot_id = Number(req.params.id);
+  const content = req.body.content;
+
+  const { data, error } = await db
+    .from("default_bots")
+    .select()
+    .eq("id", bot_id)
+    .single();
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  const response = await openai.responses.create({
+    instructions: data.instructions,
+    model: process.env.OPENAI_MODEL,
+    input: content,
+  });
+
+  const output = response.output_text;
+
+  return res.status(200).json({ response: output });
 });
 
 export default router;
