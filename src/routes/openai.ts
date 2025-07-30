27237bca7bb8e9440e8e23ee6e4797779f072b32
sutigit <panyanilben@gmail.com>
2025-07-30 12:26:16 +0300

    add embedding route

--- src/routes/openai.ts
+++ src/routes/openai.ts
@@ -2,18 +2,25 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import db from "../db/index";
 import openai from "../lib/openai";
+import { EmbeddingModel } from "openai/resources/embeddings";
 
 dotenv.config();
 const router = express.Router();
 
 router.post("/basic", async (req: Request, res: Response) => {
   const content = req.body.content;
-  const response = await openai.responses.create({
-    model: process.env.OPENAI_MODEL,
-    input: content,
-  });
 
-  const output = response.output_text;
+  let output = "";
+  try {
+    const response = await openai.responses.create({
+      model: process.env.OPENAI_MODEL,
+      input: content,
+    });
+
+    output = response.output_text;
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 
   return res.status(200).json({ response: output });
 });
@@ -32,15 +39,40 @@ router.post("/bot/:id", async (req: Request, res: Response) => {
     return res.status(500).json({ error: error.message });
   }
 
-  const response = await openai.responses.create({
-    instructions: data.instructions,
-    model: process.env.OPENAI_MODEL,
-    input: content,
-  });
+  let output = "";
+  try {
+    const response = await openai.responses.create({
+      instructions: data.instructions,
+      model: process.env.OPENAI_MODEL,
+      input: content,
+    });
 
-  const output = response.output_text;
+    output = response.output_text;
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 
   return res.status(200).json({ response: output });
+});
+
+router.post("/embedding", async (req: Request, res: Response) => {
+  const content = req.body.content;
+
+  let embedding = [];
+
+  try {
+    const res = await openai.embeddings.create({
+      model: process.env.OPENAI_EMBEDDING_MODEL as EmbeddingModel,
+      input: content,
+      encoding_format: "float",
+    });
+
+    embedding = res.data[0].embedding;
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+
+  return res.status(200).json({ embedding });
 });
 
 export default router;
